90f15487b9ed00b2c3431c766e8de38b705ce684
ravazquez <ravazquez@gmail.com>
2019-06-24 17:58:33 -0600

    Initial VL draft with working Nuitrack and RGB (Reactive) nodes and an Overview help patch

---
+++ src/ColorFrameEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using nuitrack;
+using System;
+
+namespace VL.Devices.Nuitrack
+{
+    public class ColorFrameEventArgs : EventArgs
+    {
+        public readonly ColorFrame ColorFrame;
+        
+        public ColorFrameEventArgs(ColorFrame colorFrame)
+        {
+            ColorFrame = colorFrame;
+        }
+    }
+}
